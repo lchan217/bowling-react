a07737d6e971be2075b241ebeb25fe5713cc40d6
Lizzie <lchan217@gmail.com>
2020-02-04 15:40:27 -0700

    add rough sketch of display

--- src/GameStats.js
+++ src/GameStats.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
+import { Grid, Dropdown } from "semantic-ui-react";
 
 class GameStats extends Component {
   render() {
-    return <div>game stats</div>;
+    return (
+      <div>
+        <h1>Total Score: </h1>
+        <h4>How Many Pins To Knock Down: </h4>
+        <h4>
+          Score:{" "}
+          <Dropdown text='Pick A Number'>
+            <Dropdown.Menu>
+              <Dropdown.Item text='1' />
+              <Dropdown.Item text='2' />
+              <Dropdown.Item text='3' />
+            </Dropdown.Menu>
+          </Dropdown>
+        </h4>
+        <Grid>
+          <Grid.Column key={1}>1</Grid.Column>
+          <Grid.Column key={2}>2</Grid.Column>
+          <Grid.Column key={3}>3</Grid.Column>
+          <Grid.Column key={4}>4</Grid.Column>
+          <Grid.Column key={5}>5</Grid.Column>
+          <Grid.Column key={6}>6</Grid.Column>
+          <Grid.Column key={7}>7</Grid.Column>
+          <Grid.Column key={8}>8</Grid.Column>
+          <Grid.Column key={9}>9</Grid.Column>
+          <Grid.Column key={10}>10</Grid.Column>
+        </Grid>
+      </div>
+    );
   }
 }
 
